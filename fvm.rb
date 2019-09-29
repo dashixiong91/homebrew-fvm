cae7d169afa60b4fd2cc202de375e2bd0105bed7
qianxinfeng <qianxinfeng@meituan.com>
2019-09-29 20:39:04 +0800

    version:1.0.2

--- fvm.rb
+++ fvm.rb
@@ -1,8 +1,8 @@
 class Fvm < Formula
   desc "Flutter SDK versions Manager"
   homepage "https://github.com/xinfeng-tech/fvm"
-  url "https://github.com/xinfeng-tech/fvm/archive/v1.0.1.tar.gz"
-  sha256 "a4f3ce9a309938dafafb256257516cd2c6226711800bcb23a3e3300358aaa463"
+  url "https://github.com/xinfeng-tech/fvm/archive/v1.0.2.tar.gz"
+  sha256 "5d919fd900f318b657eeb7fc31e9117828d4d3cb12fcfa24c5f5ee2014b56402"
   head "https://github.com/xinfeng-tech/fvm.git"
 
   bottle :unneeded
@@ -21,11 +21,9 @@ class Fvm < Formula
     configuration file:
 
       export FVM_DIR="$HOME/.fvm"
-      [ -s "#{opt_prefix}/init.sh" ] && \. "#{opt_prefix}/init.sh"  # This loads fvm
+      \. "#{opt_prefix}/init.sh"  # This loads fvm
 
-    You can set $FVM_DIR to any location, but leaving it unchanged from
-    #{prefix} will destroy any fvm-installed Flutter installations
-    upon upgrade/reinstall.
+    You can set $FVM_DIR to any location.
 
     Type `fvm help` for further information.
   EOS
@@ -35,6 +33,5 @@ class Fvm < Formula
     output = pipe_output("#{prefix}/fvm-init 2>&1")
     assert_no_match /No such file or directory/, output
     assert_no_match /fvm: command not found/, output
-    assert_match "", output
   end
 end
